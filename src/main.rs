50bc98e55a88892f5d6de83b79209268b698b15b
AlphaProphecy <srowbotham@me.com>
2023-10-08 21:53:32 +0100

    Added posts table and a basic get route, using pooled connections

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 #[macro_use] extern crate rocket;
 
+use rocket_sync_db_pools::{database, diesel};
+use rocket::serde::json::Json;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use models::Post;
+
+use diesel::MysqlConnection;
+
+mod models;
+mod schema;
+
+#[database("mysql")]
+pub struct Db(MysqlConnection);
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, worlds!"
@@ -7,10 +21,28 @@ fn index() -> &'static str {
 
 #[get("/test/<name>")]
 fn test(name: &str) -> String {
-    format!("Hello name, {}!", name)
+    format!("Hello sssssssssssssname, {}!", name)
+}
+
+#[get("/posts")]
+async fn get_posts(conn: Db) -> Json<Vec<Post>> {
+    use schema::posts::dsl::*;
+
+    let results = conn.run(|c| {
+        posts
+            .filter(published.eq(true))
+            .select(Post::as_select())
+            .get_results(c)
+            .expect("Error loading posts")
+    }).await;
+
+    Json(results)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, test])
+    dotenv().ok();
+    rocket::build()
+        .attach(Db::fairing())
+        .mount("/", routes![index, test, get_posts])
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,2 +1,11 @@
 // @generated automatically by Diesel CLI.
 
+diesel::table! {
+    posts (id) {
+        id -> Integer,
+        #[max_length = 255]
+        title -> Varchar,
+        body -> Text,
+        published -> Bool,
+    }
+}
